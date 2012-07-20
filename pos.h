5ee79f70ee291baeb6e1346fb72c93e4d8639b84
cosmo_ray <matthias.gatto@epitech.eu>
2012-07-20 22:43:26 +0200

    add direction to pos

--- pos.h
+++ pos.h
@@ -1,10 +1,17 @@
 #ifndef	POS_H_
 #define	POS_H_
 
+#define	NORTH	0
+#define	EAST	1
+#define	WEST	0
+#define	SOUTH	1
+
+
 typedef	struct	s_pos
 {
   int		x;
   int		y;
+  int		dir;
 }	t_pos;
 
 #endif
