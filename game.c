751d196f7cc99bada9e33e7db4d8828df48651e9
matthias <matthias.gatto@epitech.eu>
2012-08-01 17:50:54 +0200

    + game.c

---
+++ game.c
@@ -0,0 +1,34 @@
+#include	<unistd.h>
+#include	<stdio.h>
+#include	"game.h"
+
+extern t_game	life;
+
+int	give_life()
+{
+  t_ship	*ship;
+
+  if ((ship = malloc(sizeof(*ship))) == NULL)
+    return (-1);
+  init_list(&(life.ships));
+  if (load_ship("battle_cruse.sp", ship))
+    return (-1);
+  push_elem(&(life.ships), new_elem(ship));
+  if (sdl_init(500, 500) == -1)
+    return (-1);
+  load_sprite();
+  return (0);
+}
+
+void	take_life()
+{
+  t_ship	*ture;
+
+  sdl_uninit();  
+  while(life.ships.len)
+    {
+      ture = pop_data(&(life.ships));
+      destroy_ship(ture);
+      free(ture);
+    }
+}
