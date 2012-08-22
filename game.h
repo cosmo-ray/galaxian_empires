a303a89026edac385679225435c760b6d8a60c7c
cosmo_ray <matthias.gatto@epitech.eu>
2012-08-22 20:41:21 +0200

    * include sprite.h in game.h

--- game.h
+++ game.h
@@ -4,6 +4,7 @@
 #include	"sdl_util.h"
 #include	"player.h"
 #include	"bmap.h"
+#include	"sprite.h"
 
 /*contain all the vital info for the game, that should be crast at the begin of the game and destriy at the end*/
 typedef	struct	s_game
