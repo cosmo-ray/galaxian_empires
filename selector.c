7468f16fde82767e46cd78d9ef827dfabe8320e6
cosmo_ray <matthias.gatto@epitech.eu>
2012-08-22 21:40:16 +0200

    not even a game, i want to make it

--- selector.c
+++ selector.c
@@ -1,14 +1,7 @@
 #include	"selector.h"
 
-int	select_case(t_spos *pos)
-{
-  SDL_Event event;
-
-  pos->x = 0;
-  pos->y = 0;
-  while (!0)
-    {
-      SDL_WaitEvent(&event);
+/*memo of old debug*/
+/*
       switch (event.type)
 	{
 	case SDL_MOUSEBUTTONDOWN:
@@ -17,6 +10,49 @@ int	select_case(t_spos *pos)
 	  break;
 	case SDL_KEYDOWN:
 	  printf("the %s touch wat touch, his sym his: %d\n", SDL_GetKeyName(event.key.keysym.sym), event.key.keysym.sym);
+	  break;
+	case SDL_QUIT:
+	  return (1);
+	}
+ */
+
+void	display_pos(t_spos *pos)
+{
+  display_somethink(pos->x, pos->y, 0, SELECTOR);
+}
+
+int	select_case(t_spos *pos)
+{
+  SDL_Event event;
+
+  pos->x = 0;
+  pos->y = 0;
+  while (!0)
+    {
+      display_pos(pos);
+      SDL_WaitEvent(&event);
+      switch (event.type)
+	{
+	case SDL_MOUSEBUTTONDOWN:
+	  break;
+	case SDL_KEYDOWN:
+	  switch (event.key.keysym.sym)
+	    {
+	    case SDLK_UP:
+	      pos->y -= 1;
+	      break;
+	    case SDLK_DOWN:
+	      pos->y += 1;
+	      break;
+	    case SDLK_RIGHT:
+	      pos->x += 1;
+	      break;
+	    case SDLK_LEFT:
+	      pos->x -= 1;
+	      break;
+	    default:
+	      break;
+	    }
 	  break;
 	case SDL_QUIT:
 	  return (1);
