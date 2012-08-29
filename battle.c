e85f8d203d9f09b668763468ed893845d1681bfd
cosmo_ray <matthias.gatto@epitech.eu>
2012-08-29 18:34:16 +0200

    add end battle

--- battle.c
+++ battle.c
@@ -44,13 +44,29 @@ static int	battle_loop(t_battle * bd)
   return (0);
 }
 
-static void	pos_fleets_on_map(t_player *p, t_bmap *map, int	dir)
+static int	pos_fleets_on_map(t_player *p, t_bmap *map, int	dir)
 {
   /*maybe one day the loop who ask to the player where he want to pos his fleets will be implement here*/
+
   if (dir == NORTH)
     pos_fleet((t_fleet *)p->fleets.first->data, map->x / 2 - 1, 0, SOUTH);
   else
     pos_fleet((t_fleet *)p->fleets.first->data, map->x / 2 - 1, map->y - 1, NORTH);
+  return (0);
+}
+
+static	int	pos_players_fleets_on_map(t_battle *bd)
+{
+  int	ret;
+
+  ret = pos_fleets_on_map(bd->p1, &bd->map, NORTH);
+  return (ret);
+}
+
+static	int	end_battle(t_battle *bd)
+{
+  destroy_bmap(&bd->map);
+  return (0);
 }
 
 int	battle(t_player *p1, t_player *p2)
@@ -59,12 +75,12 @@ int	battle(t_player *p1, t_player *p2)
 
   init_battle_data(p1, p2, &battle_data);
   if (p1->fleets.len == 0 || p2->fleets.len == 0)
-    return (-1);
-  pos_fleets_on_map(p1, &battle_data.map, NORTH);
+    return (0);
+  if (pos_players_fleets_on_map(&battle_data))
+    return (end_battle(&battle_data));
   /* pos_player_fleets(p1, &battle_data.map, NORTH); */
   /* pos_player_fleets(p2, &battle_data.map, SOUTH); */
   display_map(&battle_data);
   battle_loop(&battle_data);
-  destroy_bmap(&(battle_data.map));
-  return (0);
+  return (end_battle(&battle_data));
 }
