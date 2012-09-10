6f45643aa459f188b94ca6a8d31e1bd49599a519
matthias <matthias.gatto@epitech.eu>
2012-09-10 19:25:08 +0200

    add comment for menu_bok

--- menu_box.h
+++ menu_box.h
@@ -1,6 +1,9 @@
 #ifndef MENU_BOX_H
 #define MENU_BOX_H
 
+/*
+ * print a char** tab, and wait for a selection of a line in the char** corespondin* to a line.
+ */
 int	print_menu(char **menu);
 
 #endif
