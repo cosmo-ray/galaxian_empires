bbac16db82cca53848a73eb072f0a129d836e7e4
matthias <matthias.gatto@epitech.eu>
2012-09-27 16:10:22 +0200

    add prototype to selector.h

--- selector.h
+++ selector.h
@@ -9,5 +9,6 @@ int	select_empty_case(t_spos *pos, t_battle *bd);
 int	select_fleet(t_spos *pos, t_battle *bd);
 int	select_ally_fleet(t_spos *pos, t_battle *bd, t_player *p);
 int	select_enemy_fleet(t_spos *pos, t_battle *bd, t_player *p);
+int	select_y_limit_case(t_spos *pos, t_battle *bd, int low_y, int hight_y);
 
 #endif
